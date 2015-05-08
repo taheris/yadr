2b0c775e08893defc81cfa5d8425df178a1009eb
Shaun Taheri <st@optiopay.com>
2015-05-08 10:14:30 +0200

    remove depth limit from du

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -30,7 +30,7 @@ alias cls='clear;ls'
 
 # Show human friendly numbers and colors
 alias df='df -h'
-alias du='du -h -d 2'
+alias du='du -h'
 
 if [[ $platform == 'linux' ]]; then
   alias ll='ls -lh --color=auto'
