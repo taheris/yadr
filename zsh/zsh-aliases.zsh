44192e0d1144a6f69e1e046baf69db63b239c395
Shaun Taheri <shaun.taheri@here.com>
2018-07-26 15:13:23 +0200

    Redefine H alias as --help

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -2,17 +2,17 @@
 alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
-alias -g C='| wc -l'
-alias -g H='| head'
-alias -g L="| less"
-alias -g N="| /dev/null"
-alias -g S='| sort'
-alias -g G='| grep' # now you can do: ls foo G something
+#alias -g C='| wc -l'
+#alias -g H='| head'
+#alias -g L="| less"
+#alias -g N="| /dev/null"
+#alias -g S='| sort'
+#alias -g G='| grep' # now you can do: ls foo G something
 
 # Functions
 #
 # (f)ind by (n)ame
-# usage: fn foo 
+# usage: fn foo
 # to find all files containing 'foo' in the name
 function fn() { ls **/*$1* }
 
